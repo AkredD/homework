661ffcbdde90c14cfa00a3f96192281c621b0c8b
AkredD <akredd@yandex.ru>
2016-05-20 03:03:35 +0300

    Add files via upload

---
+++ task3/rational.cpp
@@ -0,0 +1,37 @@
+#include "rational.h"
+
+
+rational::rational(int numer) {
+	this->numer = numer;
+	this->denom = 1;
+}
+
+rational::rational(int numer, int denom) : numer(numer), denom(denom) {
+	int gcd = calcGCD(numer, denom);
+	this->numer = numer / gcd;
+	this->denom = denom / gcd;
+}
+
+int rational::calcGCD(int l, int r) const{
+	return r ? calcGCD(r, l % r) : l;
+}
+
+rational const rational::operator-(rational const &second) const{
+	return rational(this->numer * second.denom - this->denom * second.numer,
+		this->denom * second.denom);
+}
+
+rational const rational::operator +(rational const &second) const{
+	return rational(this->numer * second.denom + this->denom * second.numer,
+		this->denom * second.denom);
+}
+
+rational const rational::operator *(rational const &second) const{
+	return rational(this->numer * second.numer,
+		this->denom * second.denom);
+}
+
+rational const rational::operator /(rational const &second) const{
+	return rational(this->numer * second.denom,
+		this->denom * second.numer);
+}
