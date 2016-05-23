24acd1f5388c255a064124818bba3a36436e84cb
AkredD <akredd@yandex.ru>
2016-05-23 17:35:21 +0300

    Update main.c

--- task1/main.c
+++ task1/main.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[]) {
     int n;
     scanf("%d", &n);
     int i, j;
+    if (n < 1) return 0;
     int **arr = (int **) malloc((n + 1) * sizeof(int *));
     for (i = 0; i <= n; i++) {
         arr[i] = (int *) malloc((n + 1) * sizeof(int));
@@ -25,6 +26,13 @@ int main(int argc, char *argv[]) {
             return 0;
         }
         scanf("%d%d%d", &y1, &x2, &y2);
+        if ( x1 < 1 || x2 < 1 || y1 < 1 || y2 < 1){
+            for (i = 0; i <= n; i++) {
+                free(arr[i]);
+            }
+            free(arr);
+            return 0;
+        }
         for (i = y1; i <= y2; ++i) {
             for (j = x1; j <= x2; ++j) {
                 printf("%d ", arr[i][j]);
